0982a40e04626f52b4473a5d0eaac111ac871e25
Carlos Daniel <carlosdanielalves2006@gmail.com>
2024-03-20 17:09:50 -0300

    Atualizaçao dos exercicos de python

---
+++ Exercicios/Cálculo de circunferência.py
@@ -0,0 +1,18 @@
+print ('')
+print (' ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨')
+print ('Cálculo de Circunferência')
+print ('----------------------------')
+print ('Solicitando entrada de dados')
+print ('----------------------------')
+r=float (input ('Informe o valor do raio: '))
+print ('')
+print ('|------------------------------|')
+print ('| CALCULANDO ÁREA DA CIRCUNFERÊNCIA |')
+print ('|------------------------------|')
+c=2*3,14*r
+print ('')
+print ('O resultado da circunferência é: ',c)
+print ('')
+print (' *** FIM DO CÁLCULO *** ')
+print ('')
+
